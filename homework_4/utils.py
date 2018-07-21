c0908be9d2de3d9319a58bc21a8f2c4b30224532
aleksey <a.egorov@daiteco.ru>
2018-07-21 18:12:22 +0300

    update

--- homework_4/utils.py
+++ homework_4/utils.py
@@ -34,12 +34,17 @@ def mnist(batch_size=50, valid=0, shuffle=True, transform=mnist_transform, path=
         train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
         return train_loader, test_loader
 
+def mnist_plotdata(transform=mnist_transform, path='./MNIST_data'):
+    train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
+    images = [train_data[i][0] for i in range(50)]
+    return images
+
 
 def plot_mnist(images, shape):
     fig = plt.figure(figsize=shape[::-1], dpi=80)
     for j in range(1, len(images) + 1):
         ax = fig.add_subplot(shape[0], shape[1], j)
-        ax.matshow(images[j - 1, 0, :, :], cmap = matplotlib.cm.binary)
+        ax.matshow(images[j - 1][0], cmap=matplotlib.cm.binary)
         plt.xticks(np.array([]))
         plt.yticks(np.array([]))
     plt.show()
