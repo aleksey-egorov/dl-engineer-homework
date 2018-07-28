5247872d7bf32659c9644d4f51b2368cd08bab2d
aleksey <a.egorov@daiteco.ru>
2018-07-28 09:30:01 +0300

    update

--- homework_5/wiki_utils.py
+++ homework_5/wiki_utils.py
@@ -1,5 +1,6 @@
 import os
 import torch
+from torch.utils.data import DataLoader
 
 
 class Alphabet(object):
@@ -85,27 +86,102 @@ class TextDataset(torch.utils.data.Dataset):
     valid_file = 'valid.txt'
     test_file = 'test.txt'
     
-    def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
+    def __init__(self, root, train=True, sequence_length=32):
         self.root = os.path.expanduser(root)
-        self.transform = transform
-        self.target_transform = target_transform
+        self.sequence_length = sequence_length
         self.train = train  # training set or test set
-
-        if download:
-            self.download()
+        self.dictionary = Alphabet()
 
         if not self._check_exists():
-            raise RuntimeError('Dataset not found.' +
-                               ' You can use download=True to download it')
+            raise RuntimeError('Dataset not found.')
 
         if self.train:
-            print (os.path.join(self.root, self.training_file))
-            self.train_data = open(os.path.join(self.root, self.training_file), 'r')
-            self.valid_data = open(os.path.join(self.root, self.valid_file), 'r')
+            self.train_data = self.tokenize(os.path.join(self.root, self.training_file))
+            self.valid_data = self.tokenize(os.path.join(self.root, self.valid_file))
         else:
-            self.test_data = open(os.path.join(self.root, self.test_file), 'r')
-        
+            self.test_data = self.tokenize(os.path.join(self.root, self.test_file))
+
+    def tokenize(self, path):
+        """Tokenizes a text file."""
+        assert os.path.exists(path)
+        # Add symbol to the dictionary
+        with open(path, 'r') as f:
+            tokens = 0
+            for line in f:
+                tokens += len(line)
+                for s in line:
+                    self.dictionary.add_symbol(s)
+
+        # Tokenize file content
+        with open(path, 'r') as f:
+            ids = torch.LongTensor(tokens)
+            token = 0
+            for line in f:
+                for s in line:
+                    ids[token] = self.dictionary.symbol2idx[s]
+                    token += 1
+
+        return ids
+
+    def __len__(self):
+        if self.train:
+            return len(str(self.train_data))
+        else:
+            return len(str(self.test_data))
+
+    def get_seq(self, dataset):
+        for i in range(0, dataset.size(0) - 1, self.sequence_length):
+            seq_len = min(self.sequence_length, len(dataset) - 1 - i)
+            data = dataset[i:i + seq_len]
+            target = dataset[i + 1:i + 1 + seq_len].view(-1)
+            yield data, target
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        texts = []
+        targets = []
+        for batch, (data_seq, target_seq) in enumerate(self.get_seq(self.train_data)):
+            texts.append(data_seq)
+            targets.append(target_seq)
+
+
+        #if self.train:
+        #    text, target = self.train_data[index], self.train_data[index]
+        #else:
+        #    text, target = self.test_data[index], self.test_data[index]
+
+        return texts, targets
+
     def _check_exists(self):
         return os.path.exists(os.path.join(self.root, self.training_file)) and \
             os.path.exists(os.path.join(self.root, self.test_file))
-    
+
+
+
+def text_dataloader(path='./text', batch_size=128, valid=0):
+    test_data = TextDataset(path, train=False)
+    test_loader = DataLoader(test_data, batch_size=batch_size)
+
+    train_data = TextDataset(path, train=True)
+    if valid > 0:
+        num_train = len(train_data)
+        indices = list(range(num_train))
+        split = num_train - valid
+
+        train_idx, valid_idx = indices[:split], indices[split:]
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+
+        train_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
+        valid_loader = DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler)
+
+        return train_loader, valid_loader, test_loader
+    else:
+        train_loader = DataLoader(train_data, batch_size=batch_size)
+        return train_loader, test_loader
